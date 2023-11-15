ebf8cbb41622171b95d075fc16a5f28efd5f3e29
Sávio Jailson Rodrigues Costa <saviojrc.1988@gmail.com>
2023-11-15 15:05:40 -0300

    Ajuste api

--- services/api_service.py
+++ services/api_service.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from util.file_util import FileUtil
 
 file_util = FileUtil()
-livros = file_util.get_payload()
+livros = file_util.get_payload()['livros']
 
 app = Flask(__name__)
 
@@ -11,16 +11,16 @@ class APIService:
     @staticmethod
     @app.route('/api/v1/livros', methods=['GET'])
     def get_livros():
-        return jsonify({'livros': livros})
-
-
+        return jsonify(livros), 200
 
     @staticmethod
-    @app.route('/api/v1/livros/<int:id>', methods=['GET'])
-    def obter_livro_por_id(id: int):
-        for livro in livros:
-            if livro.get("id") == id:
-                return jsonify({'livro': livro})
+    @app.route('/api/v1/livros/<int:codigo>', methods=['GET'])
+    def obter_livro_por_id(codigo: int):
+        livro = next((livro for livro in livros if livro['codigo'] == codigo), None)
+        if livro:
+            return jsonify(livro), 200
+        else:
+            return jsonify({'message': 'Livro não encontrado'}), 404
 
 
 app.run(port=5000, debug=True, host='localhost')
